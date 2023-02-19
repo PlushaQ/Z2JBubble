89b0fa2e5e980dff3662d3dc76c3d95c78dd5491
Maciej Fularz <fularzmaciej@gmail.com>
2023-02-19 19:31:37 +0100

    few changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def bubble_sort(lst):
     if sorted(lst) == lst:
-        return "List already sorted"
+        print("List already sorted")
+        return
     n = len(lst)
     for x in range(n):
         for y in range(n-1):
             if lst[y] > lst[y+1]:
                 lst[y], lst[y+1] = lst[y+1], lst[y]
-    print(lst)
+    print(f'Your sorted list is {lst}')
 
 
+bubble_sort([1, 4, 6, 3, 2, 5])
